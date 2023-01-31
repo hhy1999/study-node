5df824fb01afc74cc5fbd8971db02b479fa6b3a4
hhy1999 <1992395918@qq.com>
2023-01-31 14:58:48 +0800

    SECOND TEST

---
+++ 04.标准IO以及输出对象/io.js
@@ -0,0 +1,14 @@
+console.time("test");
+
+for(i=0;i<1000;i++){
+    var test = "www.jshaman.com";
+}
+
+console.timeEnd("test")
+
+
+// console.info(test)
+// console.log(test)
+// console.trace(test)
+// console.time(test)
+// console.timeEnd(test)
